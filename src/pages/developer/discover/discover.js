3fca0fddb7899fb68024550df2c94d3e4e11b073
AnasAlmasri <anas7.cule@gmail.com>
2024-06-27 15:22:12 +0800

    refactor: Fetch top-rated products in DiscoverPage
    Refactor the DiscoverPage component to fetch the top-rated products from the API. Use the `useEffect` hook to make the API call and update the `topRated` state with the response data. Handle error cases and display a message when no top-rated products are available. This change improves the functionality and data accuracy of the DiscoverPage component.

--- src/pages/developer/discover/discover.js
+++ src/pages/developer/discover/discover.js
@@ -1,10 +1,28 @@
 import InvoCarousel from "src/components/carousel/invo-carousel";
 import ProductSlider from "src/components/product-slider/product-slider";
-import prodDatajson from "src/data/products.json";
 import "src/variables.css";
 import classes from "./discover.module.css";
+import React from "react";
 
-function DiscoverPage() {
+const DiscoverPage = React.memo(() => {
+  const [topRated, setTopRated] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/api/v1/cms/app/top-rated`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTopRated((prev) => [...data]);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }, []);
+
   return (
     <div className={classes.container}>
       <InvoCarousel
@@ -16,38 +34,21 @@ function DiscoverPage() {
           "https://i.ibb.co/cQ1rkPT/000000-text-Dummy-header-test.png",
         ]}
       />
-      <ProductSlider
-        titleText={"Top Rated"}
-        productData={prodDatajson.products}
-      />
+      {topRated && topRated.length > 0 ? (
+        <ProductSlider
+          titleText={"Top Rated"}
+          productData={topRated.length > 0 ? topRated : []}
+        />
+      ) : (
+        <p>No top rated products available</p>
+      )}
       <br /> <br />
-      <ProductSlider
-        titleText={"Most Used"}
-        productData={prodDatajson.products}
-      />
-      <br /> <br />
-      <ProductSlider
-        titleText={"Rising Stars"}
-        productData={prodDatajson.products}
-      />
-      <br /> <br />
-      <ProductSlider
-        titleText={"New and Noteworthy"}
-        productData={prodDatajson.products}
-      />
-      <br /> <br />
-      <ProductSlider
-        titleText={"Explore"}
-        productData={prodDatajson.products}
-        pagination={false}
-        itemsPerPage={18}
-      />
+      {/* Uncomment other ProductSliders as needed */}
       <div className={classes.explorebutton}>
-        {" "}
         <button>View more</button>
       </div>
     </div>
   );
-}
+});
 
 export default DiscoverPage;
