963706f477f6c93eefb8c8c4cd7159a6aad7b265
oyen-engineering <31314792+AnasAlmasri@users.noreply.github.com>
2024-08-08 11:12:10 +0800

    cleanup

--- src/components/product-page-components/product-carousel/product-carousel.jsx
+++ src/components/product-page-components/product-carousel/product-carousel.jsx
@@ -1,35 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import classes from "./product-carousel.module.css";
 import { Carousel } from "rsuite";
 
-import { ReactComponent as UpDownArrow } from "src/public/svg/UP-Down_Arrow.svg";
-
-const imageStyle = (width, height) => ({
-  maxWidth: width,
-  maxHeight: height,
-});
-
 function ProductCarousel({ images, height, width }) {
   const [activeIndex, setActiveIndex] = useState(0);
-
-  useEffect(() => {
-    console.log(images);
-  }, [images]);
-  function rightButton() {
-    if (activeIndex == images.length - 1) {
-      setActiveIndex(0);
-    } else {
-      setActiveIndex(activeIndex + 1);
-    }
-  }
-
-  function leftButton() {
-    if (activeIndex == 0) {
-      setActiveIndex(images.length - 1);
-    } else {
-      setActiveIndex(activeIndex - 1);
-    }
-  }
 
   return Array.isArray(images) && images.length > 0 ? (
     <div className={classes.container} style={{maxWidth: width}}>
@@ -53,14 +27,8 @@ function ProductCarousel({ images, height, width }) {
         {images.slice(0, 5).map((image, index) => {
           return (
             <div className={index === activeIndex ? classes.activeimage : ''}  style={{aspectRatio: width/ height}}  onClick={() => setActiveIndex(index)}>
-
-            <img
-              src={image}
-              alt=""
-            
-            />
-                      </div>
-
+              <img src={image} alt="" />
+            </div>
           );
         })}
       </div>
